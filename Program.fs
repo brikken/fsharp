303675ae0583ceeee9e1fbab8017874844114f67
Mads Nielsen <post@madsnielsen.dk>
2019-10-13 11:37:43 +0200

    fun with Computation Expressions

--- Program.fs
+++ Program.fs
@@ -3,15 +3,34 @@
 open System
 open State
 
+let init = Ok 1
+
+let dummy : Result<int,int> =
+    match init with
+    | Error error -> Error error
+    | Ok a ->
+        printfn "We passed the init!"
+        let b = a + 1
+        printfn "b is %i" b
+        match Error b with
+        | Error error -> Error error
+        | Ok c ->
+            printfn "We passed the C!"
+            Ok c
 
 [<EntryPoint>]
 let main argv =
+    match dummy with
+    | Ok a -> printfn "Ok %i" a
+    | Error b -> printfn "Error %i" b
     let dummy =
         result {
-            //let! a = Ok 1
-            let! a = Error 1
+            let! a = init
+            printfn "We passed the init!"
             let b = a + 1
+            printfn "b is %i" b
             let! c = Error b
+            printfn "We passed the C!"
             return c
         }
     match dummy with
