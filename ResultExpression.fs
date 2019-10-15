67d805a13a25a16dd3d7f313d3c5084d6fe69547
Mads Nielsen <post@madsnielsen.dk>
2019-10-15 23:03:36 +0200

    TFIO read and write, but lacks rollback

--- ResultExpression.fs
+++ ResultExpression.fs
@@ -1,5 +1,5 @@
 [<AutoOpen>]
-module Result
+module ResultExpression
 
 open System
 
@@ -7,33 +7,21 @@ let ofOption error = function Some s -> Ok s | None -> Error error
 
 type ResultBuilder() =
     member __.Return(x) =
-        printfn "return"
         Ok x
 
     member __.ReturnFrom(m: Result<_, _>) = m
 
-    member __.Bind(result, binder) =
-        printfn "bind beginning"
-        match result with
-            | Error e ->
-                printfn "bind ending in error"
-                Error e
-            | Ok x ->
-                let r = binder x
-                printf "ok bind ending in "
-                match r with
-                | Ok _ -> printfn "ok"
-                | Error _ -> printfn "error"
-                r
+    member __.Bind(result, binder) = Result.bind binder result
+
     member __.Bind((m, error): (Option<'T> * 'E), f) = m |> ofOption error |> Result.bind f
 
     member __.Zero() = None
 
     member __.Combine(m, f) = Result.bind f m
 
-    // member __.Delay(f: unit -> _) = f
+    member __.Delay(f: unit -> _) = f
 
-    // member __.Run(f) = f()
+    member __.Run(f) = f()
 
     member __.TryWith(m, h) =
         try __.ReturnFrom(m)
@@ -51,7 +39,7 @@ type ResultBuilder() =
         do f() |> ignore
         __.While(guard, f)
 
-    // member __.For(sequence:seq<_>, body) =
-    //     __.Using(sequence.GetEnumerator(), fun enum -> __.While(enum.MoveNext, __.Delay(fun () -> body enum.Current)))
+    member __.For(sequence:seq<_>, body) =
+        __.Using(sequence.GetEnumerator(), fun enum -> __.While(enum.MoveNext, __.Delay(fun () -> body enum.Current)))
 
 let result = ResultBuilder()
--- TFIO.fs
+++ TFIO.fs
@@ -1,14 +1,30 @@
 module TFIO
 
+open ResultExpression
+
+type TFIODirectory = TFIODirectory of string
 type TFIOPath = TFIOPath of string
 
 type TFIOLock = IOLock of TFIOPath
 
+type TFIOStream = TFIOStream of int
+
+type TFIOProviderError =
+    | FileAlreadyExists
+    | SourceFileDoesNotExist
+    | DestinationFileDoesNotExist
+    | IOError of string
+
 type TFIOProvider = {
-    read: TFIOPath -> byte []
-    write: (TFIOPath * byte []) -> unit
-    lock: TFIOPath -> TFIOLock
+    ``open``: TFIOPath -> Result<TFIOStream,TFIOProviderError>
+    close: TFIOStream -> unit
+    read: TFIOStream -> Result<byte [],TFIOProviderError>
+    write: TFIOStream -> byte [] -> Result<unit,TFIOProviderError>
+    move: TFIOPath -> TFIOPath -> Result<unit,TFIOProviderError>
+    replace: TFIOPath -> TFIOPath -> TFIOPath -> Result<unit,TFIOProviderError>
+    lock: TFIOPath -> Result<TFIOLock,TFIOProviderError>
     unlock: TFIOLock -> unit
+    getBackupPath: TFIOPath -> TFIOPath
 }
 
 type Action =
@@ -17,7 +33,9 @@ type Action =
 
 type State = Action list
 
-type TFIOError = string
+type TFIOError =
+    | TFIOError of string
+    | TFIOProviderError of TFIOProviderError
 
 type TFIO<'a> = TFIO of (State * TFIOProvider -> State * Result<'a,TFIOError>)
 
@@ -44,7 +62,7 @@ let tfio = TFIOBuilder
 let rollback : State -> Result<unit,'b> = fun state ->
     Ok ()
 
-let readFile : TFIOProvider -> TFIOPath -> State -> Result<byte [],string> = fun io path state ->
+let readFile : TFIOProvider -> TFIOPath -> State -> Result<byte [],TFIOProviderError> = fun io path state ->
     let prevWriteContents logItem =
         match logItem with
         | WriteAction (writePath, contents) when writePath = path -> Some contents
@@ -52,41 +70,79 @@ let readFile : TFIOProvider -> TFIOPath -> State -> Result<byte [],string> = fun
     match state |> List.rev |> List.tryPick prevWriteContents with
     | Some contents -> Ok contents
     | None ->
-        try
-            Ok (io.read path)
-        with
-            ex -> Error ex.Message
+        result {
+            let! stream = io.``open`` path
+            let! contents = io.read stream
+            do io.close stream
+            return contents
+        }
 
 let readFileTFIO = fun path ->
     let inner (state, prov) =
         match readFile prov path state with
         | Ok contents -> (ReadAction (path, contents)::state, Ok contents )
-        | Error e -> (state, Error e)
+        | Error e -> (state, Error e |> Result.mapError TFIOProviderError)
     TFIO inner
+
+// TODO: This needs to be more robust, ie. ensure no invalid filenames are returned
+let getTempPath : TFIOPath -> System.Guid -> TFIOPath = fun path guid ->
+    let (TFIOPath path') = path
+    TFIOPath (path' + "." + (sprintf "%8s" (guid.ToString())))
+
+let getTempFileStream : TFIOProvider -> TFIOPath -> Result<TFIOPath * TFIOStream,TFIOProviderError> = fun prov path ->
+    let rec getTempFileStreamRec () =
+        let pathTemp = getTempPath path (System.Guid.NewGuid())
+        match prov.``open`` pathTemp with
+        | Ok stream -> Ok (pathTemp, stream)
+        | Error FileAlreadyExists -> getTempFileStreamRec ()
+        | Error e -> Error e
+    getTempFileStreamRec ()
+
+let moveOrReplace : TFIOProvider -> TFIOPath -> TFIOPath -> TFIOPath -> Result<unit,TFIOProviderError> = fun prov src dst bkp ->
+    let rec moveOrReplaceRec () =
+        match prov.move src dst with
+        | Ok _ -> Ok ()
+        | Error FileAlreadyExists ->
+            match prov.replace src dst bkp with
+            | Ok _ -> Ok ()
+            | Error DestinationFileDoesNotExist -> moveOrReplaceRec ()
+            | Error e -> Error e
+        | Error e -> Error e
+    moveOrReplaceRec ()
+
+// TODO: This comp. expr. should keep a log of what's been done, such that it can roll it back. This is a mini TFIO-expr. in itself
+let writeFile : TFIOProvider -> TFIOPath -> byte [] -> Result<unit,TFIOProviderError> = fun prov path contents ->
+    result {
+        let! (pathTemp, stream) = getTempFileStream prov path
+        do! prov.write stream contents
+        do prov.close stream
+        do! moveOrReplace prov pathTemp path (prov.getBackupPath path)
+    }
 
 let writeFileTFIO = fun path contents ->
-    let inner (state, _) =
-        (WriteAction (path, contents)::state, Ok () )
+    let inner (state, prov) =
+        (WriteAction (path, contents)::state, writeFile prov path contents |> Result.mapError TFIOProviderError)
     TFIO inner
 
-let lockStateFiles : TFIOProvider -> State -> Result<TFIOLock list,string> = fun io state ->
-    // TODO: When more actions are added, this needs to carry a state, to keep track of already locked files, moved files, deleted files, etc.
-    let fileToLock a =
-        match a with
-        | ReadAction (path,_) -> [ path ]
-        | WriteAction (path,_) -> [ path ]
+// TODO: When more actions are added, this needs to carry a state, to keep track of already locked files, moved files, deleted files, etc.
+let fileToLock a =
+    match a with
+    | ReadAction (path,_) -> [ path ]
+    | WriteAction (path,_) -> [ path ]
+
+let lockStateFiles : TFIOProvider -> State -> Result<TFIOLock list,TFIOProviderError> = fun io state ->
     let lockFileFolder s t =
         match s with
         | Ok locks ->
-            try
-                Ok ((io.lock t)::locks)
-            with
-                ex ->
-                    locks |> List.map io.unlock |> ignore
-                    Error ex.Message
-        | _ -> s
+            match io.lock t with
+            | Ok lock -> Ok (lock::locks)
+            | Error e ->
+                locks |> List.map io.unlock |> ignore
+                Error e
+        | Error e -> Error e
     state
     |> List.collect fileToLock
+    |> List.distinct
     |> List.fold lockFileFolder (Ok [])
     |> Result.map List.rev
 
